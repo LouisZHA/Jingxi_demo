eb3d7daf04da6bf471265f5a10da6b06d44ae01d
LouisZHA <824603549zdy@gmail.com>
2023-07-31 18:20:50 +0800

    first commit

---
+++ src/main/java/com/capgemini/jingxi_demo/JingxiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.jingxi_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingxiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingxiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capgemini/jingxi_demo/JingxiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.jingxi_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingxiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

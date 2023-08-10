c7dc9fdbd60c4eae695811cecdee040fb6746ea9
LouisZHA <824603549zdy@gmail.com>
2023-08-10 18:41:22 +0800

    fix bug

--- src/main/java/com/capgemini/jingxi_demo/application/products/dto/mapping/ProductMapping.java
+++ src/main/java/com/capgemini/jingxi_demo/application/products/dto/mapping/ProductMapping.java
@@ -3,18 +3,20 @@ package com.capgemini.jingxi_demo.application.products.dto.mapping;
 import com.capgemini.jingxi_demo.application.products.dto.ProductAddDTO;
 import com.capgemini.jingxi_demo.infrastructure.entity.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapping {
 
-//    @Mapping(source = "id", target = "idDTO")
-//    @Mapping(source = "name", target = "nameDTO")
-//    @Mapping(source = "description", target = "descriptionDTO")
-//    @Mapping(source = "price", target = "idDTO")
-//    @Mapping(source = "quantity", target = "realNameDTO")
+//    @Mapping(source = "id", target = "id")
+//    @Mapping(source = "name", target = "name")
+//    @Mapping(source = "description", target = "description")
+//    @Mapping(source = "price", target = "id")
+//    @Mapping(source = "quantity", target = "realName")
+
     ProductEntity toProductEntity(ProductAddDTO productAddDTO);
 
-    ProductAddDTO toProductAddDTO(ProductEntity productEntity);
+//    ProductAddDTO toProductAddDTO(ProductEntity productEntity);
 
 }
